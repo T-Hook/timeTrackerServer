163a217a31d8819a820d14cc6f00fe9fac30bc20
GAROUI nabiha <garouinabiha96@gmail.com>
2021-04-13 01:29:12 +0200

    13 - 04 - 2021

--- fixtures/users.js
+++ fixtures/users.js
@@ -7,8 +7,9 @@ module.exports =[
     "password": "$2a$10$vCQoABhAucQ2fiaxMZIHl.RNJJpEiJqjRIjYu20Kudacb9Ta7pl8u",
     "username": "admin@gmail.com",
     "email": "admin@gmail.com",
+    "lname": "admin",
+    "fname":"admin",
     "role": "superuser",
-    "profile": ObjectId("5d1a07b17d2dac401ecf766b"),
     "__v": 0,
     "active": true
   }
